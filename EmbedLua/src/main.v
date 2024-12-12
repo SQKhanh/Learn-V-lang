2f48841cdccfa4a075bc504cf710d607846a6a46
KhánhDz <vichuyen.123@gmail.com>
2024-12-12 07:00:32 +0700

    Create README.MD

--- EmbedLua/src/main.v
+++ EmbedLua/src/main.v
@@ -2,16 +2,11 @@ module main
 
 #flag -I"./include" // include header file
 #flag -L"."
-#flag -l"c_dll"
 #flag -l"lua54"
 
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
-
-#flag -gcc
-#flag -g
-#flag -Wall
 
 struct C.lua_State {}
 
@@ -24,7 +19,6 @@ fn C.luaL_dostring(&C.lua_State, &char) int
 fn print_hello_from_lua() {
 	println('Bắt đầu gọi lua')
 	lstate := C.luaL_newstate()
-	println('lstate: ${lstate}')
 
 	C.luaL_openlibs(lstate) // Open standard Lua libraries
 
@@ -34,11 +28,8 @@ fn print_hello_from_lua() {
 	println('Done gọi lua ?')
 }
 
-fn C.printAge(int)
-
 fn main() {
 	println('Start')
-	C.printAge(123)
 
 	print_hello_from_lua()
 }
